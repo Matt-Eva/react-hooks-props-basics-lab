ec0c8f6e33ccd19573df30114f3fab98e8869faf
Matt Eva <matteweva@gmail.com>
2021-11-02 11:44:05 -0700

    commit changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+
+  function renderBio(){
+  if(typeof props.bio === "undefined" || props.bio === "" ){
+    return null;
+  }else {
   return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+    <p>{props.bio}</p>
   );
+  }
+}
+
+return <div id="about">
+  <h2>About Me</h2>
+  {renderBio()}
+  <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+  <Links linkedin = {props.linkedin} github = {props.github}/>
+</div>
+
 }
 
 export default About;
